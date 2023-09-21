779e3ca542a1fa336b904e7d7f4b2672439fc0e6
Rodrigo Dorantes <106606020+rodrigoremen@users.noreply.github.com>
2023-09-21 15:55:59 -0600

    inicio del blog

---
+++ app/blog/page.js
@@ -0,0 +1,131 @@
+export default function blog() {
+  return (
+    <div className="mx-auto max-w-screen-xl mt-12 lg:h-screen lg:items-center">
+      <div className="mx-auto text-center">
+        <h1 className="text-3xl font-extrabold sm:text-5xl text-bold">
+          Sección de noticias
+        </h1>
+        
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 mt-32">
+          <div class="h-32 rounded-lg bg-gray-100">
+            <a href="#" class="group relative block bg-black">
+              <img
+                alt="Developer"
+                src="https://www.jornada.com.mx/ultimas/2023/09/08/amlo-arriba-a-colombia-a-reunion-con-gustavo-petro-1192.html/amlo-colombiano.jpg-1044.html/image_large?bc=2023-09-08T18:24:07-05:00"
+                class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
+
+              <div class="relative p-4 sm:p-6 lg:p-8">
+                <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
+                  Noticias
+                </p>
+
+                <p class="text-xl font-bold text-white sm:text-2xl">Acerca del pais</p>
+
+                <div class="mt-32 sm:mt-48 lg:mt-64">
+                  <div
+                    class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                  >
+                    <p class="text-sm text-white">
+                    A bordo de una aeronave de la Fuerza Aérea Mexicana, 
+                    el mandatario aterrizó alrededor de las 16:30 local 
+                    en el aeropuerto
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="h-32 rounded-lg bg-gray-100">
+            <a href="#" class="group relative block bg-black">
+              <img
+                alt="Developer"
+                src="https://www.elfinanciero.com.mx/resizer/QULmxPFCYUlNsKhf430sMskESFk=/800x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/elfinanciero/6V6GZZROIZACFNIKTZ7I4REPFM.jpg"
+                class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
+
+              <div class="relative p-4 sm:p-6 lg:p-8">
+                <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
+                  Noticias
+                </p>
+
+                <p class="text-xl font-bold text-white sm:text-2xl">Choce de aviones</p>
+
+                <div class="mt-32 sm:mt-48 lg:mt-64">
+                  <div
+                    class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                  >
+                    <p class="text-sm text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                      perferendis hic asperiores quibusdam quidem voluptates doloremque
+                      reiciendis nostrum harum. Repudiandae?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="h-32 rounded-lg bg-gray-100">
+            <a href="#" class="group relative block bg-black">
+              <img
+                alt="Developer"
+                src="https://aristeguinoticias.com/_next/image/?url=https%3A%2F%2Feditorial.aristeguinoticias.com%2Fwp-content%2Fuploads%2F2023%2F09%2Fsismo-collima-jalisco-08092023.jpg&w=1200&q=75"
+                class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
+
+              <div class="relative p-4 sm:p-6 lg:p-8">
+                <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
+                  Developer
+                </p>
+
+                <p class="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                <div class="mt-32 sm:mt-48 lg:mt-64">
+                  <div
+                    class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                  >
+                    <p class="text-sm text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                      perferendis hic asperiores quibusdam quidem voluptates doloremque
+                      reiciendis nostrum harum. Repudiandae?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="h-32 rounded-lg bg-gray-100">
+            <a href="#" class="group relative block bg-black">
+              <img
+                alt="Developer"
+                src="https://images.milenio.com/LCDT8eJryK68pdjXdMckDx9TSbg=/942x532/uploads/media/2023/09/08/abogado-uriel-carmona-traslado-penal_70_0_1140_720.jpeg"
+                class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+              />
+
+              <div class="relative p-4 sm:p-6 lg:p-8">
+                <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
+                  Noticia 1
+                </p>
+
+                <p class="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+                <div class="mt-32 sm:mt-48 lg:mt-64">
+                  <div
+                    class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                  >
+                    <p class="text-sm text-white">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+                      perferendis hic asperiores quibusdam quidem voluptates doloremque
+                      reiciendis nostrum harum. Repudiandae?
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
