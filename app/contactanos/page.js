4c6845ed7c0e7369ec2e28eceb59b3d202a46718
RubenLD <rubendl.lodo@gmail.com>
2023-09-21 23:05:15 -0600

    inicio de contactanos

---
+++ app/contactanos/page.js
@@ -0,0 +1,39 @@
+export default function Contactanos() {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="w-full shrink-0 grow-0 basis-auto lg:w-7/14 m-5">
+                                <div className="flex items-start">
+                                    <div className="ml-6 grow">
+                                        <p className="mb-2 font-bold dark:text-white">
+                                            Contáctanos
+                                        </p>
+                                        <p className="text-neutral-500 dark:text-neutral-200">
+                                        Av. Universidad Tecnológica No.1, Col. Palo Escrito, C.P. 62760
+                                        </p>
+                                        <p className="text-neutral-500 dark:text-neutral-200">
+                                        Emiliano Zapata, Morelos. Tel. (01 777) 3 68 11 65
+                                        </p>
+                                        <p className="text-neutral-500 dark:text-neutral-200">
+                                        Comunícate sin costo al 01 800 050 UTEZ(8839)
+                                        </p>
+                                    </div>
+                                </div>
+                    </div>
+
+                    <div>
+                        <div className="flex justify-center mt-4">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3775.828576851627!2d-99.20331042497726!3d18.850288459019986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdd8bad2a5ef7d%3A0xddfd0f46aac691ee!2sUniversidad%20Tecnol%C3%B3gica%20Emiliano%20Zapata%20UTEZ!5e0!3m2!1ses!2smx!4v1694216937715!5m2!1ses!2smx" width={1200} height={450} style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    );
+}
